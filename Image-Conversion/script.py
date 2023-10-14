800ed7337191c899461ba7e66cfaea120ac5265b
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-10-13 22:18:40 -0400

    Made Image Converter Recursive Optional
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Image-Conversion/script.py
+++ Image-Conversion/script.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 import sys
+import os
 from PIL import Image
 
 
-def main():
-    """Script to Convert JPEG to PNG"""
+"""Script to Convert JPEG to PNG"""
+
+
+def normal():
     count = len(sys.argv)
     source = sys.argv[1]
     if count < 3:
@@ -16,5 +19,34 @@ def main():
     return 0
 
 
+def recursive():
+    source = (
+        sys.argv[2] if "~" in sys.argv[2] else os.path.expanduser(sys.argv[2])
+    )
+
+    source = source if source[-1] == "/" else source + "/"
+
+    if os.path.isdir(source):
+        files = os.listdir(source)
+        for file in files:
+            try:
+                im1 = Image.open(source + file)
+                destination = file.split(".")[0] + ".png"
+                print(destination)
+                im1.save(source + destination)
+            except:
+                print("Not an image file")
+                continue
+    else:
+        print("Not a directory")
+        return 1
+
+    return 0
+
+
 if __name__ == "__main__":
-    main()
+    print(sys.argv)
+    if sys.argv[1] == "recursive":
+        recursive()
+    else:
+        normal()
--- image-conversion.sh
+++ image-conversion.sh
@@ -1,6 +1,23 @@
 #!/bin/zsh
 
-source_arg=$1
-destination_arg=$2
+source_arg=""
+destination_arg=""
+r_flag=false
 
-python ~/Developer/Scripts/Image-Conversion/script.py $source_arg $destination_arg
+
+if [ $1 = "-r" ]; then
+  r_flag=true
+  source_arg=$2
+  destination_arg=$3
+else
+  source_arg=$1
+  destination_arg=$2
+fi
+
+
+
+if [ "$r_flag" = true ]; then
+  python ~/Developer/Scripts/Image-Conversion/script.py "recursive" $source_arg $destination_arg
+else
+  python ~/Developer/Scripts/Image-Conversion/script.py $source_arg $destination_arg
+fi
